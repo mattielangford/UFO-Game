2a7744fe16d37ff760aca7113b63b737d30efed0
Mattie Knebel-Langford <49666068+mattielangford@users.noreply.github.com>
2019-05-21 22:39:12 -0600

    Add files via upload

---
+++ ufo_functions.hpp
@@ -0,0 +1,13 @@
+#include <vector>
+#include "ufo_functions.cpp"
+
+void display_misses(int misses);
+
+// Declare functions
+
+void greet();
+void end_game(std::string answer, std::string codeword);
+void display_status(std::vector<char>incorrect, std::string answer);
+
+
+
